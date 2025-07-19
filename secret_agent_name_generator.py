4b5010eae18f5f6a189ff68e63982c40e237da9b
KingSlayer <168217552+KingSlayer9909@users.noreply.github.com>
2025-07-19 12:47:22 -0500

    Add files via upload

---
+++ secret_agent_name_generator.py
@@ -0,0 +1,18 @@
+print("Welcome, Agent")
+
+first_name= input("Enter your first Name: ")
+color=input("Pick your favorite Color: ")
+animal=input("Pick your favorite animal: ")
+number=input("Favorite number: ")
+
+#Simple Check
+if first_name.lower() == "james":
+    print("Bond? is That you?!")
+if first_name.lower() =="brandon":
+    print("Is that you FATTY!")
+
+
+# Code name output
+print("\nYour secret agent name is:")
+print(f"Agent {color.capitalize()} {animal.capitalize()}-{number.zfill(2)}")
+
